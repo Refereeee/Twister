21820994d56c596123da1a9953f697147ed5d2bf
nedojuniorr <rrefereezzz@gmail.com>
2022-03-24 17:17:24 +0300

    fourth-task

--- UI/index.js
+++ UI/index.js
@@ -48,8 +48,7 @@ const tweets = [
                 author: 'Ricardo Cromento',
             }
         ],
-    }
-    ,
+    },
     {
         id: '3',
         text: 'I can\'t wait for Nightdive to do Quake II and include the N64 version. ',
@@ -125,7 +124,6 @@ const tweets = [
             }
         ],
     },
-
     {
         id: '6',
         text: 'I just tested positive for COVID. I’ve had a scratchy throat for a couple days',
@@ -250,7 +248,8 @@ const tweets = [
                 author: 'Ricardo Cromento',
             }
         ],
-    }, {
+    },
+    {
         id: '11',
         text: 'Epic Games  know has defied the App Store Monopoly. ',
         createdAt: new Date('2022-03-01T23:25:00'),
@@ -305,7 +304,6 @@ const tweets = [
         text: 'By acting now and know how funding organizations like ',
         createdAt: new Date('2022-03-12T23:25:00'),
         author: 'Bill Gates',
-        hashTags: 'person',
         comments: [{
             id: '800',
             text: 'I look for every opportunity to talk about Doom 64s lighting. It\'s sublime on a CRT',
@@ -331,7 +329,6 @@ const tweets = [
         text: 'By acting know how funding organizations like ',
         createdAt: new Date('2022-03-10T23:15:00'),
         author: 'Bill Gates',
-        hashTags: 'person',
         comments: [{
             id: '2200',
             text: 'I look for every opportunity to talk about Doom 64s lighting. It\'s sublime on a CRT',
@@ -357,7 +354,6 @@ const tweets = [
         text: 'By acting know how like ',
         createdAt: new Date('2022-03-16T23:25:00'),
         author: 'Bill Gates',
-        hashTags: 'person',
         comments: [{
             id: '2300',
             text: 'I look for every opportunity to talk about Doom 64s lighting. It\'s sublime on a CRT',
@@ -383,7 +379,6 @@ const tweets = [
         text: 'By look for every opportunity to talk about Doom 64s lighting',
         createdAt: new Date('2022-03-17T23:20:00'),
         author: 'Bill Gates',
-        hashTags: 'person',
         comments: [{
             id: '2400',
             text: 'I look for every opportunity to talk about Doom 64s lighting. It\'s sublime on a CRT',
@@ -409,7 +404,6 @@ const tweets = [
         text: 'Telegram: the app at the heart of Ukraine’s propaganda battle',
         createdAt: new Date('2022-03-18T23:20:00'),
         author: 'Bill Gates',
-        hashTags: 'person',
         comments: [{
             id: '2500',
             text: 'I look for every opportunity to talk about Doom 64s lighting. It\'s sublime on a CRT',
@@ -435,7 +429,6 @@ const tweets = [
         text: 'Happy New Year!',
         createdAt: new Date('2022-03-11T23:20:00'),
         author: 'Bill Gates',
-        hashTags: 'person',
         comments: [{
             id: '2600',
             text: 'I look for every opportunity to talk about Doom 64s lighting. It\'s sublime on a CRT',
@@ -461,7 +454,6 @@ const tweets = [
         text: 'Public calls to violence have no place on Telegram',
         createdAt: new Date('2022-03-14T23:20:00'),
         author: 'Bill Gates',
-        hashTags: 'person',
         comments: [{
             id: '2600',
             text: 'I look for every opportunity to talk about Doom 64s lighting. It\'s sublime on a CRT',
@@ -487,7 +479,6 @@ const tweets = [
         text: 'Unlike 2013, 2020 is a year when most mobile apps (including Telegram) are best used on an Android phone.',
         createdAt: new Date('2022-03-14T23:20:00'),
         author: 'Bill Gates',
-        hashTags: 'person',
         comments: [{
             id: '2600',
             text: 'I look for every opportunity to talk about Doom 64s lighting. It\'s sublime on a CRT',
@@ -508,175 +499,378 @@ const tweets = [
             }
         ],
     },
+    {
+        id: '21',
+        text: 'Unlike 2011, 2019 is a year when most mobile apps (including Telegram) are best used on an Android phone.',
+        comments: [{
+            id: '2800',
+            text: 'I look for every opportunity to talk about Doom 64s lighting. It\'s sublime on a CRT',
+            createdAt: new Date('2022-03-19T23:20:05'),
+            author: 'Bill Gates',
+        },
+            {
+                id: '2801',
+                text: 'Doom 64 actually had a really cool aesthetic. For what was supposed to be a spinoff/port, i',
+                createdAt: new Date('2022-03-19T23:23:15'),
+                author: 'Daniel Adams',
+            },
+            {
+                id: '2802',
+                text: 'Doom 64 actually had a really cool aesthetic. For what was supposed to be a spinoff/port, i',
+                createdAt: new Date('2022-03-19T23:25:25'),
+                author: 'Ricardo Cromento',
+            }
+        ],
+    },
 ];
 
 
 
+class Comment {
 
-const myModule = (function () {
-    let user = "Bill Gates";
+    static maxValueLength = 280;
 
-    let tweetsState = [...tweets];
-
-    const sorting = (skip=0,top=10) => {
-        return tweetsState.sort((a, b) => a.createdAt - b.createdAt).slice(skip, skip + top)
+    static validateStructure = {
+        id: (val) => typeof val === 'string' && val.trim().length,
+        createdAt: (val) => typeof val === 'object' && val instanceof Date,
+        text: (val) => typeof val === 'string' && val.length <= Comment.maxValueLength && val.length,
+        author: (val) => typeof val === 'string' && val.trim().length,
     }
 
-    const filterByAuthor = () => {
+    static validate(comment){
+        for(const [key,value] of Object.entries(comment)){
+            if(!Object.keys(Comment.validateStructure).includes(key)){
+                return false
+            }
+            if(!Comment.validateStructure[key](value)){
+                return false
+            }
+        }
+        if(!Object.keys(Comment.validateStructure).every(key => Object.keys(comment).includes(key))){
+            return false
+        }
+        return true
+    }
+
+    constructor(options) {
+
+        if(!Comment.validate(options)){
+            throw new Error('Comment validate failed')
+        }
+
+        this._id = options.id
+        this._createdAt = options.createdAt
+        this._author = options.author
+        this._text = options.text
+    }
+
+    get id() {
+        return this._id
+    }
+
+
+    get createdAt() {
+        return this._createdAt
+    }
+
+
+    get author() {
+        return this._author;
+    }
+
+
+    get text() {
+        return this._text
+    }
+
+}
+
+const comment = new Comment({
+    id: "50000",
+    createdAt: new Date('2022-03-03T23:55:25'),
+    author: "Bill Gates",
+    text: "Public calls to violence have no place on Telegram",
+})
+
+console.log(comment)
+
+class Tweet {
+
+    static maxValueLength = 280;
+
+    static validateStructure = {
+        id: (val) => typeof val === 'string' && val.trim().length,
+        createdAt: (val) => typeof val === 'object' && val instanceof Date,
+        text: (val) => typeof val === 'string' && val.length <= Tweet.maxValueLength && val.length,
+        author: (val) => typeof val === 'string' && val.trim().length,
+        comments: (val) => Array.isArray(val)
+    }
+
+    static validate(tweet){
+        for(const [key,value] of Object.entries(tweet)){
+            if(!Object.keys(Tweet.validateStructure).includes(key)){
+                return false
+            }
+            if(!Tweet.validateStructure[key](value)){
+                return false
+            }
+        }
+        if(!Object.keys(Tweet.validateStructure).every(key => Object.keys(tweet).includes(key))){
+            return false
+        }
+        return true
+    }
+
+    constructor(options) {
+
+        if(!Tweet.validate(options)){
+            throw new Error('Tweet validate failed')
+        }
+
+        this._id = options.id
+        this._createdAt = options.createdAt
+        this._author = options.author
+        this._text = options.text
+        this._comments = options.comments
+    }
+
+    get id() {
+        return this._id
+    }
+
+
+    get createdAt() {
+        return this._createdAt
+    }
+
+
+    get author() {
+        return this._author;
+    }
+
+
+    get text() {
+        return this._text
+    }
+
+    get comments (){
+        return this._comments
+    }
+
+}
+
+// const tweet = new Tweet({
+//     id: "35",
+//     createdAt: new Date('2022-03-02T23:55:25'),
+//     text: "Public calls to violence have no place on Telegram",
+//     author: "Bill Gates",
+//     comments: []
+// })
+//
+// console.log(tweet)
+
+
+
+
+
+
+class TweetFeed {
+
+    _list =[]
+
+    static maxValueLength = 280;
+    static validateUser(user) {
+        return typeof user === 'string' && user.trim().length;
+    }
+
+    constructor(user,tws) {
+
+        if(!TweetFeed.validateUser(user)){
+            throw new Error ('Validate user error')
+        }
+        this._user = user
+        this._tweets = tws
+
+    }
+
+    get user (){
+        return this._user
+    }
+
+    filterByAuthor() {
         if (filterConfig?.author) {
-            return tweetsState = tweetsState.filter(el => el.author === user);
+            return this._tweets = this._tweets.filter(el => el.author === this._user);
         }
     }
 
-    const filterByDate = () => {
+    filterByDate() {
         if (filterConfig?.dateFrom && filterConfig?.dateTo) {
-            return tweetsState = tweetsState.filter(el => el.createdAt >= filterConfig.dateFrom && el.createdAt <= filterConfig.dateTo)
+            return this._tweets = this._tweets.filter(el => el.createdAt >= filterConfig.dateFrom && el.createdAt <= filterConfig.dateTo)
         }
         if (filterConfig?.dateFrom) {
-            return tweetsState = tweetsState.filter(el => el.createdAt > filterConfig.dateFrom)
+            return this._tweets = this._tweets.filter(el => el.createdAt > filterConfig.dateFrom)
         }
         if (filterConfig?.dateTo) {
-            return tweetsState = tweetsState.filter(el => el.createdAt < filterConfig?.dateTo)
+            return this._tweets = this._tweets.filter(el => el.createdAt < filterConfig?.dateTo)
         }
     }
 
 
-    const filterByHashtags = () => {
-        if (filterConfig?.hashTags) {
-            return tweetsState = tweetsState.filter(el => {
-                if (filterConfig.hashTags.includes(el.hashTags)) {
-                    return el
-                }
-            })
-        }
-    }
 
-    const filterByText = () => {
+
+    filterByText() {
         if (filterConfig?.text) {
-            return tweetsState = tweetsState.filter(el => {
+            return this._tweets = this._tweets.filter(el => {
                 if (el.text.includes(filterConfig.text)) {
                     return el
                 }
             })
         }
     }
+    sorting(skip = 0, top = 10) {
+        return this._tweets = this._tweets.sort((a, b) => a.createdAt - b.createdAt).slice(skip, skip + top)
+    }
 
 
-    const getTweets = (skip = 0, top = 10, filterConfig = {}) => {
-        if (filterConfig === {}) {
-            return sorting(skip,top)
-        }
-
-        if (filterConfig !== undefined) {
-            filterByAuthor()
-            filterByDate()
-            filterByHashtags()
-            filterByText()
-            return sorting(skip,top)
+    getPage(skip = 0, top = 10, filterConfig = {}) {
+        if (Object.keys(filterConfig).length === 0) {
+            return this.sorting(skip, top)
+        } else {
+            this.filterByAuthor()
+            this.filterByDate()
+            this.filterByText()
+            return this.sorting(skip, top)
         }
     }
 
-    const getTweet = id => tweets.find(el => el.id === `${id}`);
+    get(id) {
+        return tweets.find(el => el.id === `${id}`)
+    }
 
-    const validateTweet = tweet => {
-        if (typeof tweet.id === "string" && typeof tweet.createdAt === "object" && (typeof tweet.text === "string" && tweet.text.length <= 280) && (typeof tweet.author === "string" && tweet.author.length > 0) && Array.isArray(tweet.comments)) {
-            return true
+    add(text) {
+
+        const date = new Date();
+        const options = {
+            id: String(+date),
+            text,
+            createdAt:date,
+            author: this._user,
+            comments:[]
+        };
+
+        if(!Tweet.validate(options)) {
+            return false
         }
-        return false
+
+        const newTweet = new Tweet(options);
+        this._tweets.push(newTweet);
+
+        return true
     }
 
-    const validateComments = com => {
-        if (typeof com.id === "string" && typeof com.createdAt === "object" && (typeof com.text === "string" && com.text.length <= 280) && (typeof com.author === "string" && com.author.length > 0)) {
-            return true
-        }
-        return false
-    }
+    edit(id, text) {
+        const editObj = this.get(id);
 
-    const addTweet = text => {
-        const newTweet = new Object();
-        newTweet.id = String(+(new Date()));
-        newTweet.createdAt = new Date();
-        newTweet.author = user;
-        newTweet.text = text;
-        newTweet.comments = [];
-        if (validateTweet(newTweet)) tweets.push(newTweet)
-    }
-
-    const editTweet = (id, text) => {
-        const editObj = getTweet(id);
-        if (validateTweet(editObj) && editObj.author === user && (typeof text === "string" && text.length <= 280)) {
+        if (Tweet.validate(editObj) && editObj.author === this._user && (typeof text === "string" && text.length <= TweetFeed.maxValueLength)) {
             editObj.text = text;
             return true
         }
+
         return false
     }
 
-    const removeTweet = id => {
-        const objRemove = getTweet(id);
-        const numInArr = tweets.findIndex(el => el.id === id)
-        if (objRemove.author === user) {
-            tweets.splice(numInArr, 1)
+    remove(id) {
+
+        const objRemove = this.get(id);
+        const numInArr = this._tweets.findIndex(el => el.id === id)
+
+        if (objRemove.author === this._user) {
+            this._tweets.splice(numInArr, 1)
             return true
         }
+
         return false
     }
 
-    const addComment = (id, text) => {
-        const comArr = getTweet(id).comments;
-        console.log(comArr);
-        const comObj = new Object();
-        comObj.id = String(+(new Date()));
-        comObj.text = text;
-        comObj.createdAt = new Date();
-        comObj.author = user;
-        if (validateComments(comObj)) {
-            comArr.push(comObj)
-            return true
+    addComment(id, text) {
+
+        const comArr = this.get(id).comments;
+        const date = new Date();
+
+        const options = {
+            id: String(+date),
+            text,
+            createdAt: date,
+            author: this._user
+        };
+
+        if(!Comment.validate(options)) {
+            return false
+
         }
-        return false
+
+        const newComment = new Comment(options);
+        comArr.push(newComment);
+        return true
     }
 
+    changeUser(user) {
 
-    const changeUser = (usr) => user = usr
+        if(!TweetFeed.validateUser(user)) {
+            throw new Error('Validate user error')
+        }
 
+        this._user = user;
+    }
 
+    addAll(tws){
 
-    return {
-        filterByAuthor,
-        filterByDate,
-        filterByHashtags,
-        filterByText,
-        sorting,
-        getTweets,
-        getTweet,
-        addTweet,
-        editTweet,
-        removeTweet,
-        addComment,
-        changeUser,
-    };
+        const garbage = [];
 
+        if(!Array.isArray(tws)){
+            throw new Error('Error of tweets is not array')
+        }
 
-})()
+        for(const tw of tws) {
+            if(typeof tw !== 'object' || !Tweet.validate(tw)){
+                garbage.push(tw)
+            }
+            else{
+                this._list.push(tw)
+            }
+        }
+        return garbage
+    }
+
+    clear(){
+        this._list = [];
+    }
+
+    get list (){
+        return this._list
+    }
+
+    get tweets (){
+        return this._tweets
+    }
+}
 
 const filterConfig = {
     author: "Bill Gates",
     dateFrom: new Date('2022-03-02T23:55:25'),
     dateTo: new Date('2022-03-15T23:55:25'),
-    hashTags: ['newbie', 'person'],
     text: 'know'
 }
+const tf = (new TweetFeed("Bill Gates",tweets));
+console.log(tf.getPage())
 
 
-console.log(myModule.getTweets())
-
-console.log(myModule.myeditTweet(13, 'new fallen'))
-console.log(myModule.getTweet(13))
-console.log(myModule.removeTweet("13"))
-console.log(tweets);
-console.log(myModule.addComment(5, 'i feel you'));
-console.log(myModule.getTweet(5));
-console.log(myModule.changeUser("Barak Obama"));
-
+// console.log(tf._edit('13','fast'));
+// console.log(tf.get('13'));
+// console.log(tf._remove('13'));
+// console.log(tf._addComment('13','aiaiai'));
+console.log(tf.changeUser("Vasya"));
+// console.log(tf._addAll(tweets))
+// console.log(tf.tweets);
 
 
